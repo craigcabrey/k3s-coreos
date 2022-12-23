45413c193f3f8016e70239bcf39e6ce06393e104
Craig Cabrey <craigcabrey@gmail.com>
2022-12-23 16:37:16 -0600

    automatically substitute variables

--- update-ignition-configs.sh
+++ update-ignition-configs.sh
@@ -3,7 +3,7 @@
 source "$(pwd)/secrets"
 
 for file in $(ls ignition-configs/*.bu); do
-  envsubst '$CLUSTER_CIDR,$SERVICE_CIDR,$K3S_TOKEN' < "$file" | butane -o "${file%.*}.ign"
+  envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < "$file" | butane -o "${file%.*}.ign"
 done
 
 kubectl create configmap nginx-static-content --from-file ignition-configs -o yaml --dry-run=client | kubectl apply -f -
